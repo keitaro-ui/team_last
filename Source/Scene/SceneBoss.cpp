6d1ede075fef12962ca897ffab4bcbde7a6c497b
keitaro-ui <kei.kei.taro305@gmail.com>
2026-05-20 14:26:57 +0900

    バトルシステムの概要完成

--- Source/Scene/SceneBoss.cpp
+++ Source/Scene/SceneBoss.cpp
@@ -3,16 +3,29 @@
 #include <CameraController.h>
 #include <Camera.h>
 #include <System/Input.h>
+#include <ctime>
 
 void SceneBoss::Initialize()
 {
-	//ステージ初期化
+	//random準備
+	srand((unsigned int)time(nullptr));
+
+	//ステージ初期設定
 	stage = std::make_unique<Stage>(1);
 	stage->SetPosition(DirectX::XMFLOAT3(10, -5, 10));
 
-	//プレイヤー初期化
+	//プレイヤー初期設定
 	player = std::make_unique<Player>();
 	player->SetPosition({ -22.8f, 1.0f, -26.0f });
+	emp.hp = 1000;
+	emp.special = 150;
+	SetPlayerPunch();
+	SetPlayerKick();
+
+	//エネミー初期設定
+	pre.hp = 1000;
+	pre.punch = 50;
+	pre.kick = 100;
 
 	//カメラ初期設定
 	Graphics& graphics = Graphics::Instance();
@@ -32,12 +45,26 @@ void SceneBoss::Initialize()
 	cameraController = std::make_unique<CameraController>();
 	player->cameraController = cameraController.get();
 
+	// スクリーンサイズ取得
+	screenWidth = Graphics::Instance().GetScreenWidth();
+	screenHeight = Graphics::Instance().GetScreenHeight();
+
+	//sprite初期設定
+	triangle_black = std::make_unique<Sprite>("Data/Sprite/BLACK_TRIANGLE.png");
+	frames[0] = std::make_unique<Sprite>("Data/Sprite/page1.png");
+	frames[1] = std::make_unique<Sprite>("Data/Sprite/page2.png");
+	frames[2] = std::make_unique<Sprite>("Data/Sprite/page3.png");
+	frames[3] = std::make_unique<Sprite>("Data/Sprite/page4.png");
+
+	//変数宣言
+	state = NONE;
+	slideSize = screenWidth / 3;
+	upSlideSize = 0.0f;
 }
 
 void SceneBoss::Finalize()
 {
 	Input::Instance().GetMouse().Unlock();
-
 }
 
 void SceneBoss::Update(float elapsedTime)
@@ -54,16 +81,66 @@ void SceneBoss::Update(float elapsedTime)
 	//プレイヤー更新処理
 	player->Update(elapsedTime);
 
-	//debugのマウスカーソル
-	if (!player->GetPrev())
+	// ルーレット開始、resultに結果をintで返す
+	if (isRoulette)
 	{
-		// マウス位置の取得とロック
-		Input::Instance().GetMouse().Lock();
-		cameraController->MouseCamera(elapsedTime);
+		resultHitCount = BossRoulette(elapsedTime, commands);
 	}
-	else
+
+	// ルーレットの中身の関数
+	if (isRouletteStop)
+		rouletteResult(roulette);
+
+	// レベルアップ
+	levelUp(elapsedTime);
+
+	// spacekey処理
+	cooltime += elapsedTime;
+
+	GamePad& gamePad = Input::Instance().GetGamePad();
+	if (gamePad.GetButtonDown() & GamePad::BTN_START && cooltime > 1.0f)
 	{
-		Input::Instance().GetMouse().Unlock();
+		if (state == START)
+		{
+			isRoulette = true;
+			isLevelUp = false;
+			state = STOP;
+		}
+		else if (state == STOP)
+		{
+			isRouletteStop = true;
+		}
+		else if (state == NONE)
+		{
+			attack = true;;
+			state = START;
+		}
+		cooltime = 0.0f;
+	}
+
+	// debug
+	{
+		// 操作
+		/*if (gamePad.GetButtonDown() & GamePad::BTN_START)
+		{
+			isRouletteStop = true;
+		}
+		if (gamePad.GetButtonDown() & GamePad::BTN_A)
+		{
+			isRoulette = true;
+		}*/
+	
+		// マウスカーソル
+		if (!player->GetPrev())
+		{
+			// マウス位置の取得とロック
+			Input::Instance().GetMouse().Lock();
+			cameraController->MouseCamera(elapsedTime);
+		}
+		else
+		{
+			Input::Instance().GetMouse().Unlock();
+		}
 	}
 }
 
@@ -104,48 +181,456 @@ void SceneBoss::Render()
 
 	// 2Dスプライト描画
 	{
+		// サイズをまとめる
+		DirectX::XMINT3 size = { screenWidth / 3 * 2, screenHeight / 3, 0 };
 
+		// ルーレット
+		if (isRoulette)
+		{
+			triangle_black->Render(rc,
+				screenWidth / 3 * 2 - screenWidth / 12, screenHeight / 3.1f + screenHeight / 11.8f * rouletteIndex, 0,
+				75, 70, 0,
+				1, 1, 1, 1);
+
+			frames[nowLevel]->Render(rc,
+				size.x, size.y, size.z,
+				screenWidth / 5, screenHeight / 5 * 3, 0,
+				1, 1, 1, 1);
+		}
+
+		// レベルアップ
+		if (isLevelUp)
+		{
+			int nextLevel = nowLevel + 1;
+
+			frames[nowLevel]->Render(rc,
+				size.x - upSlideSize, size.y, size.z,
+				screenWidth / 5, screenHeight / 5 * 3, 0,
+				1, 1, 1, 1);
+
+			frames[nextLevel]->Render(rc,
+				size.x - upSlideSize + slideSize, size.y, size.z,
+				screenWidth / 5, screenHeight / 5 * 3, 0,
+				1, 1, 1, 1);
+		}
 	}
 }
 
 void SceneBoss::DrawGUI()
 {
+	{
+		ImGui::Begin("Roulette Debug");
+
+		ImGui::Text("rouletteIndex : %d", rouletteIndex);
+
+		ImGui::Text("resultIndex : %d", resultIndex);
+
+		ImGui::Text("rouletteTimer : %f", rouletteTimer);
+
+		ImGui::Text("rouletteInterval : %f", rouletteInterval);
+
+		ImGui::Text("isRouletteStop : %s",
+			isRouletteStop ? "true" : "false");
+
+		ImGui::Text("isRoulette : %s",
+			isRoulette ? "true" : "false");
+
+		ImGui::Text("isLevelUp : %s",
+			isLevelUp ? "true" : "false");
+
+		ImGui::DragInt("slide", &slide, 1);
+
+		ImGui::Text("state : %d", state);
+
+		ImGui::Text("cooltime : %d", cooltime);
+
+		ImGui::End();
+	}
+
+	{
+		ImGui::Begin("Character Status");
+
+		ImGui::Text("Employee");
+		ImGui::Separator();
+
+		ImGui::DragInt("Emp HP", &emp.hp, 1);
+
+		ImGui::DragInt("Emp Punch", &emp.punch, 1);
+
+		ImGui::DragInt("Emp Kick", &emp.kick, 1);
+
+		ImGui::DragInt("Emp Special", &emp.special, 1);
+
+		ImGui::Spacing();
+
+		ImGui::Text("President");
+		ImGui::Separator();
+
+		ImGui::DragInt("Pre HP", &pre.hp, 1);
+
+		ImGui::DragInt("Pre Punch", &pre.punch, 1);
+
+		ImGui::DragInt("Pre Kick", &pre.kick, 1);
+
+		ImGui::End();
+	}
 }
 
-void SceneBoss::RenderUI()
+int SceneBoss::BossRoulette(float elapsedTime, int maxCount)
 {
-	// スクリーンサイズ取得
-	float screenWidth = Graphics::Instance().GetScreenWidth();
-	float screenHeight = Graphics::Instance().GetScreenHeight();
+	rouletteTimer += elapsedTime;
 
-	Camera& camera = Camera::Instance();
-	DirectX::XMMATRIX view = DirectX::XMLoadFloat4x4(&camera.GetView());
-	DirectX::XMMATRIX projection = DirectX::XMLoadFloat4x4(&camera.GetProjection());
-	DirectX::XMMATRIX world = DirectX::XMMatrixIdentity();
+	if (rouletteTimer >= rouletteInterval)
+	{
+		rouletteTimer = 0.0f;
 
-	DirectX::XMFLOAT3 plyPos = player.get()->GetPosition();
-	plyPos.y += 2.0f;
+		rouletteIndex++;
 
-	DirectX::XMVECTOR worldPlyPos = DirectX::XMLoadFloat3(&plyPos);
+		if (rouletteIndex >= maxCount)
+		{
+			rouletteIndex = 0;
+		}
+	}
 
-	DirectX::XMVECTOR screenPlyPos, screenPly2Pos;
-	screenPlyPos = DirectX::XMVector3Project(
-		worldPlyPos,
-		0.0f, 0.0f, screenWidth, screenHeight,
-		0.0f, 1.0f, projection, view, world
-	);
+	// 停止開始
+	if (isRouletteStop)
+	{
+		// 徐々に減速
+		rouletteInterval += 0.001f;
 
-	DirectX::XMFLOAT2 screenPlyPosition;
-	DirectX::XMStoreFloat2(&screenPlyPosition, screenPlyPos);
+		// 最初の1回だけ結果決定
+		if (resultIndex == -1)
+		{
+			//resultIndex = rand() % maxCount;
+			resultIndex = 0;
+			roulette = resultIndex;
+		}
 
-	//ゲージ描画
-	const float gaugeWidth = 120.0f;
-	const float gaugeHeight = 20.0f;
+		// 結果位置に来たら停止 敵モーション終わってからに変更予定
+		if (rouletteIndex == resultIndex &&
+			rouletteInterval >= 0.45f)
+		{
+			isRoulette = false;
+			isRouletteStop = false;
+			resultIndex = -1;
+			rouletteInterval = 0.1f;
+		}
+	}
 
-	/*spriteUI->Render(rc,
-		-4.946f, 2.0f, -14.102f,
-		10.0f,10.
-		0,
-		0, 1, 1, 1
-	);*/
+	return rouletteIndex;
+}
+
+void SceneBoss::rouletteResult(int result)
+{
+	if (nowLevel == 0)
+	{
+		switch (result)
+		{
+		case 0:
+			// 敵パンチ
+			if (attack)
+			{
+				emp.hp -= pre.punch;
+				attack = false;;
+			}
+			state = NONE;
+			break;
+
+		case 1:
+			// 次へ
+			slide = nowLevel + 1;
+			isLevelUp = true;
+			isRoulette = false;
+			break;
+
+		case 2:
+			// 踊り
+			state = NONE;
+			break;
+
+		case 3:
+			// 次へ
+			slide = nowLevel + 1;
+			isLevelUp = true;
+			isRoulette = false;
+			break;
+
+		case 4:
+			// 敵パンチ
+			if (attack)
+			{
+				emp.hp -= pre.punch;
+				attack = false;
+			}
+			state = NONE;
+			break;
+
+		case 5:
+			// プレイヤーパンチ
+			if (attack)
+			{
+				pre.hp -= emp.punch;
+				attack = false;
+			}
+			state = NONE;
+			break;
+
+		case 6:
+			// 踊り
+			state = NONE;
+			break;
+		}
+	}
+	else if (nowLevel == 1)
+	{
+		switch (result)
+		{
+		case 0:
+			// 次へ
+			slide = nowLevel + 1;
+			isLevelUp = true;
+			isRoulette = false;
+			break;
+
+		case 1:
+			// 踊り
+			state = NONE;
+			break;
+
+		case 2:
+			// プレイヤ―キック
+			if (attack)
+			{
+				pre.hp -= emp.kick;
+				attack = false;
+			}
+			state = NONE;
+			break;
+
+		case 3:
+			// プレイヤ―キック
+			if (attack)
+			{
+				pre.hp -= emp.kick;
+				attack = false;
+			}
+			state = NONE;
+			break;
+
+		case 4:
+			// 敵キック
+			if (attack)
+			{
+				emp.hp -= pre.kick;
+				attack = false;
+			}
+			state = NONE;
+			break;
+
+		case 5:
+			// 次へ
+			slide = nowLevel + 1;
+			isLevelUp = true;
+			isRoulette = false;
+			break;
+
+		case 6:
+			// 敵キック
+			if (attack)
+			{
+				emp.hp -= pre.kick;
+				attack = false;
+			}
+			state = NONE;
+			break;
+		}
+	}
+	else if (nowLevel == 2)
+	{
+		switch (result)
+		{
+		case 0:
+			// プレイヤ―キック
+			if (attack)
+			{
+				pre.hp -= emp.kick;
+				attack = false;
+			}
+			state = NONE;
+			break;
+
+		case 1:
+			// 敵パンチ
+			if (attack)
+			{
+				emp.hp -= pre.punch;
+				attack = false;
+			}
+			state = NONE;
+			break;
+
+		case 2:
+			// プレイヤーパンチ
+			if (attack)
+			{
+				pre.hp -= emp.punch;
+				attack = false;
+			}
+			state = NONE;
+			break;
+
+		case 3:
+			// 次へ
+			slide = nowLevel + 1;
+			isLevelUp = true;
+			isRoulette = false;
+			break;
+
+		case 4:
+			// 敵キック
+			if (attack)
+			{
+				emp.hp -= pre.kick;
+				attack = false;
+			}
+			state = NONE;
+			break;
+
+		case 5:
+			// プレイヤ―キック
+			if (attack)
+			{
+				pre.hp -= emp.kick;
+				attack = false;
+			}
+			state = NONE;
+			break;
+
+		case 6:
+			// 敵パンチ
+			if (attack)
+			{
+				emp.hp -= pre.punch;
+				attack = false;
+			}
+			state = NONE;
+			break;
+		}
+	}
+	else if (nowLevel == 3)
+	{
+		switch (result)
+		{
+		case 0:
+			// 必殺
+			if (attack)
+			{
+				pre.hp -= emp.special;
+				attack = false;
+			}
+			state = NONE;
+			break;
+
+		case 1:
+			// 踊り
+			state = NONE;
+			break;
+
+		case 2:
+			// 必殺
+			if (attack)
+			{
+				pre.hp -= emp.special;
+				attack = false;
+			}
+			state = NONE;
+			break;
+
+		case 3:
+			// 次へ
+			slide = nowLevel + 1;
+			isLevelUp = true;
+			isRoulette = false;
+			break;
+
+		case 4:
+			// 踊り
+			state = NONE;
+			break;
+
+		case 5:
+			// 必殺
+			if (attack)
+			{
+				pre.hp -= emp.special;
+				attack = false;
+			}
+			state = NONE;
+			break;
+
+		case 6:
+			// 踊り
+			state = NONE;
+			break;
+		}
+	}
+	//state = NONE;
+}
+
+void SceneBoss::levelUp(float elapsedTime)
+{
+	// オーバーロードを防ぎ、最初にもどす
+	if (nowLevel == commands - 1)
+	{
+		nowLevel = 0;
+	}
+
+	// レベルアップ終了処理
+	if (slideSize < upSlideSize)
+	{
+		isLevelUp = false;
+		upSlideSize = 0.0f;
+		nowLevel += 1;
+		state = START;
+	}
+	// レベルアップ開始、slideSizeまで増やしていく
+	else if (isLevelUp && slideSize > upSlideSize)
+	{
+		upSlideSize += elapsedTime * 500;
+	}
+
+}
+
+void SceneBoss::SetPlayerPunch()
+{
+	int level = 1;
+	if (level == 1)
+	{
+		emp.punch = 15;
+	}
+	else if (level == 2)
+	{
+		emp.punch == 30;
+	}
+	else if (level == 3)
+	{
+		emp.punch == 45;
+	}
+}
+
+void SceneBoss::SetPlayerKick()
+{
+	int level = 1;
+	if (level == 1)
+	{
+		emp.kick = 30;
+	}
+	else if (level == 2)
+	{
+		emp.kick = 50;
+	}
+	else if (level == 3)
+	{
+		emp.kick = 70;
+	}
 }
